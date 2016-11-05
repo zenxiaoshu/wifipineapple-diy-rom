84fb50093884baf9672e1506d23398ff94f740d6
XinChao Feng <wenchaoangel@gmail.com>
2016-11-05 14:25:23 -0700

    del files

--- files/etc/pineapple/landingpage.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// This page will be displayed to any clients that are browsing HTTP pages.
--- files/pineapple/api/index.php
+++
@@ -1,7 +0,0 @@
-<?php namespace pineapple;
-
-header('Content-Type: application/json');
-
-require_once('API.php');
-$api = new API();
-echo $api->magic();
